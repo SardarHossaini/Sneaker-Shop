26b6ecfd7da604577185d984947b948167515e9e
Sardar219 <sardarhossaini2022@gmail.com>
2025-05-18 12:59:04 +0430

    last changes for today

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -36,6 +36,38 @@ class _HomePageState extends State<HomePage> {
       bottomNavigationBar: MyBottomNavBar(
         onTabChange: (index) => navigateBottomNavBar(index),
       ),
+      appBar: AppBar(
+        leading: Builder(
+          builder: (context) => IconButton(
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              icon: Icon(Icons.menu_rounded)),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[900],
+        child: Column(
+          children: [
+            /// A drawer header displaying the application logo
+            ///
+            /// Uses an image asset with a white color filter, positioned within a drawer
+            /// Optionally can have a border decoration and is typically used at the top of a drawer
+            DrawerHeader(
+                // decoration: BoxDecoration(
+                //     border: Border.all(
+                //   width: 5.0,
+                // )),
+                // margin: EdgeInsets.only(bottom: 20),
+                child: Image.asset(
+              'assets/images/logo.png',
+              color: Colors.white,
+            )),
+          ],
+        ),
+      ),
       body: pages[_selectedIndex],
     );
   }
