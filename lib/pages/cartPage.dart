3ee28c15fa013a5e100344afb4c9dba2ca10988f
Sardar219 <sardarhossaini2022@gmail.com>
2025-05-19 11:43:51 +0430

    add cartItem into cartPage

--- lib/pages/cartPage.dart
+++ lib/pages/cartPage.dart
@@ -1,12 +1,38 @@
+import 'dart:js';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+import 'package:sneaker_shop/model/cart.dart';
+import 'package:sneaker_shop/model/shoe.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Cart Page"),
-    );
+    return Consumer<Cart>(
+        builder: (context, value, child) => Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Column(
+                children: [
+                  Text(
+                    "My Cart",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Expanded(
+                      child: ListView.builder(
+                          itemCount: 10,
+                          itemBuilder: (context, index) {
+                            Shoe eachShoe = value.getUserCart()[index];
+                            return CartItem();
+                          }))
+                ],
+              ),
+            ));
   }
 }
