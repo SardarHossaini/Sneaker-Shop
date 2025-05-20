7535670868bab57a5b94925ab3941aafc5a936ac
Sardar219 <sardarhossaini2022@gmail.com>
2025-05-20 11:25:40 +0430

    update favoriteTile to display discounted price if applicable

--- lib/components/favoriteTile.dart
+++ lib/components/favoriteTile.dart
@@ -30,7 +30,21 @@ class _FavoriteTileState extends State<FavoriteTile> {
               widget.shoe.imagepath,
             )),
         title: Text(widget.shoe.name),
-        subtitle: Text(widget.shoe.price),
+        subtitle: widget.shoe.discount != "0%"
+            ? Text(
+                "\$" + widget.shoe.discountPrice,
+                style: const TextStyle(
+                    fontSize: 15,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold),
+              )
+            : Text(
+                "\$" + widget.shoe.price,
+                style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
+              ),
         trailing: IconButton(
           onPressed: removeItem,
           icon: const Icon(
